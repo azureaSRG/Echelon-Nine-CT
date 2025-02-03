8d16e12d1fb61a502303c89a9c1fc314587e4e31
Silverworn <ssdsus.funtimefoxy@gmail.com>
2025-02-02 23:11:39 -0500

    TIMER
    I MADE A FRICKEN TIMER

---
+++ Main Project/Assets/Timer Script.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using TMPro;
+
+public class InGameTimer : MonoBehaviour
+{
+    public TMP_Text timerText;
+    private float elapsedTime = 0f;
+    private bool isRunning = true;
+
+    private void Update()
+    {
+        if (isRunning)
+        {
+            elapsedTime += Time.deltaTime;
+            UpdateTimerDisplay();
+        }
+    }
+
+    void UpdateTimerDisplay()
+    {
+        int minutes = Mathf.FloorToInt(elapsedTime / 60);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+   
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    public void StopTimer()
+    {
+        isRunning = false;
+    }
+
+    public void StartTimer()
+    {
+        isRunning = true;
+    }
+
+    public void ResetTimer()
+    {
+        elapsedTime = 0f;
+        UpdateTimerDisplay();
+    }
+}
