7592ef5ee356ac63a61f8de61b845fa90b84877d
zura is sleep deprived <162836474+azureaSRG@users.noreply.github.com>
2025-02-12 20:29:15 -0500

    Update PlayerStats.cs

--- Main Project/Assets/PlayerStats.cs
+++ Main Project/Assets/PlayerStats.cs
@@ -4,18 +4,34 @@ using System.Collections.Generic;
 
 public class PlayerStats : MonoBehaviour
 {
-
     [SerializeField]
     protected int health;
     [SerializeField]
     protected int maxHealth;
     [SerializeField]
     protected bool alive;
-    [SerializeField]
-    protected int experience;
-    [SerializeField]
-    protected int playerLevel;
+    public int experience;
+    public int playerLevel;
+    [SerializedField]
+    protected int neededXP;
+    private int[] xpIntervals = new int[] {1000,2000,3000,4000,5000,10000,15000,20000,30000,40000,49999};
+    //10,20,30,40,50,60,70,80,90,96,98
 
+    public void gainExperience(int xp)
+    {
+        int gainedXP = xp += experience;
+        experience = gainedXP;
+        checkLevelUp();
+    }
+    
+    public void checkLevelUp()
+    {
+        if (experience >= neededXP)
+        {
+            playerLevel++;
+        }
+    }
+    
     public void checkHealth()
     {
         if (health < 0)
