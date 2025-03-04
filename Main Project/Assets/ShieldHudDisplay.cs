2fd5962f0a82549a54208d95b65aaa7910157c4c
The Spoon King <ssdsus.funtimefoxy@gmail.com>
2025-03-04 09:53:53 -0500

    Create ShieldHudDisplay.cs
    Dude this took so long to convert into whatever the fuck this language is.

---
+++ Main Project/Assets/ShieldHudDisplay.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ShieldHUD : MonoBehavior
+{
+  public GameObject shieldPrefab;
+  public Transform shieldContainer;
+  //Set these values to be equal to the shields stat in playerStats
+  public int currentArmor = 0;
+  public int maxArmorSlots = 0;
+
+  private List<GameObject> Shields = new List<GameObject>();
+
+  void Start()
+  {
+    DrawShieldHud(currentArmor, maxArmorSlots);
+  }
+
+  void Update()
+  {
+    DrawShieldHud(currentArmor, maxArmorSlots);
+  }
+
+  void DrawShieldHud(int currentArmorPlates, int maxArmorSlots)
+  {
+    float width = 380f / maxArmorSlots;
+    float x_offset = 10f
+
+    if (Shields.Count < maxArmorSlots)
+    {
+      for (int i = Shields.Count; i < maxArmorSlots; i++)
+      {
+        GameObject armorPlate = Instantiate(shieldPrefab, shieldContainer);
+        Shields.Add(armorPlate);
+      }
+    }
+    else if (Shields.Count > maxArmorSlots)
+    {
+      for (int i = Shields.Count - 1; i >= maxArmorSlots; i--)
+      {
+        Destroy(Shields[i]);
+        Shields.RemoveAt(i);
+      }
+    }
+
+    for (int i = 0; i < maxArmorSlots; i++)
+    {
+      Image shieldImage = Shields[i].GetCompnent<Image>();
+      shieldImage.color = (i < currentArmorPlates) ? Color.cyan : Color.gray;
+
+      RectTransform rt = Shields[i].GetComponent<RectTransform>();
+      rt.sizeDelta = new Vector2(width, 20f);
+      rt.anchoredPosition = new Vector2(x_offset + i * width, 0);
+    }
+  }
+}
