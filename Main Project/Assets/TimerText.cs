dc5d6a100358f85e5ddae1c8246efb0fc91ba048
The Spoon King <ssdsus.funtimefoxy@gmail.com>
2025-04-14 11:15:40 -0400

    Update TimerText.cs
    You're welcome. I am helping by providing more "abstraction" or whatever the fuck it's called

--- Main Project/Assets/TimerText.cs
+++ Main Project/Assets/TimerText.cs
@@ -9,6 +9,7 @@ public class TimerText : MonoBehaviour
 
     void Update()
     {
+        // Checks to see if the code if the script is running and then adds to the timer
         if (isRunning)
         {
             elapsedTime += Time.deltaTime;
@@ -16,6 +17,7 @@ public class TimerText : MonoBehaviour
         }
     }
 
+    // A function that literally takes the time and formats it and assigns it to the text box
     void UpdateTimerDisplay()
     {
         int minutes = Mathf.FloorToInt(elapsedTime / 60);
@@ -24,16 +26,19 @@ public class TimerText : MonoBehaviour
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
+    // Call this to stop the timer
     public void StopTimer()
     {
         isRunning = false;
     }
 
+    // Call this to run it
     public void StartTimer()
     {
         isRunning = true;
     }
 
+    // Call this to set the time back to 0 but keep it running
     public void ResetTimer()
     {
         elapsedTime = 0f;
